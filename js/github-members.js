cfa43d0d8019466a7fbe24962ec230a0709f984c
Johanna Cherry <johanna@hailocab.com>
2013-05-07 19:59:24 +0100

    Fixing javascript for firefox.

--- js/github-members.js
+++ js/github-members.js
@@ -2,8 +2,8 @@ $(document).ready(function(){
     $.ajax({
         url: "https://api.github.com/orgs/hailocab/members"
     }).done(function(data) {
-        $(data).each(function(i){
-            $("div.container__inner").append('<a href="' + this.url + '"><img src="' + this.avatar_url + '" title="' + this.login + '"/></a>');
+        $.each($.parseJSON(data), function(i, datum){
+            $("div.container__inner").append('<a href="' + this.html_url + '"><img src="' + this.avatar_url + '" title="' + this.login + '"/></a>');
         });
     });
 });
