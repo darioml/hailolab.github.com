85f188be1fee781b070bcdbab6d23bbfe94222ca
Johanna Cherry <johanna@hailocab.com>
2013-05-10 17:05:03 +0100

    Change styles over to use the same styles as the blog page on the main web site.

--- js/github-members.js
+++ js/github-members.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
     $.getJSON("https://api.github.com/orgs/hailocab/members", function(data) {
         $(data).each(function(i){
-            $("div.container__inner").append('<a href="' + this.html_url + '"><img src="' + this.avatar_url + '" title="' + this.login + '"/></a>');
+            $("article").append('<a href="' + this.html_url + '"><img src="' + this.avatar_url + '" title="' + this.login + '"/></a>');
         });
     });
 });
