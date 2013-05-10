fe304c3f600ac0940d628ce8e374423f2d70978c
Johanna Cherry <johanna@hailocab.com>
2013-05-10 18:13:12 +0100

    Fix sticky footer problem.

--- js/github-members.js
+++ js/github-members.js
@@ -1,7 +1,8 @@
 $(document).ready(function(){
     $.getJSON("https://api.github.com/orgs/hailocab/members", function(data) {
-        $(data).each(function(i){
-            $("article").append('<a href="' + this.html_url + '"><img src="' + this.avatar_url + '" title="' + this.login + '"/></a>');
-        });
-    });
+                $("article").append('<p class="team-pics">');
+                $(data).each(function(i){
+                    $("article p.team-pics").append('<a href="' + this.html_url + '"><img src="' + this.avatar_url + '" title="' + this.login + '"/></a>');
+                });
+            });
 });
