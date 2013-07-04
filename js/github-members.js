ad5cd72ba785a9d3ea8916897827cacedd8b4b70
Martin Sweeney <martin@hailocab.com>
2013-07-04 16:46:14 +0100

    Give team images a fixed size

--- js/github-members.js
+++ js/github-members.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
     $.getJSON("https://api.github.com/orgs/hailocab/members", function(data) {
                 $("article").append('<p class="team-pics">');
                 $(data).each(function(i){
-                    $("article p.team-pics").append('<a href="' + this.html_url + '"><img src="' + this.avatar_url + '" title="' + this.login + '"/></a>');
+                    $("article p.team-pics").append('<a href="' + this.html_url + '"><img src="' + this.avatar_url + '" title="' + this.login + '" width="80" height="80" /></a>');
                 });
             });
 });
