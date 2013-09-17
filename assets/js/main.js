a2ecc978d30e92f78c55b6bd4e2bc37c004caa4e
Dario Magliocchetti <darioml@hailocab.com>
2013-09-17 14:35:34 +0100

    Font, animations and colours!

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,34 @@
 $(function() {
-    $("nav.site-nav").mouseenter(function(item){
-        $(item.target).find("a.pure-button.sub-button").each(function(count, item) {
-            setTimeout(function() { $(item).css({'opacity': 1, 'left': '0px'}) }, 15*count)
+    var show;
+
+    var funcShow = function(item){
+        if (show != true) {
+            show = true;
+            $("nav.site-nav").find("a.pure-button.sub-button").each(function(count, item) {
+                setTimeout(function() { $(item).removeClass("nav-hidden") }, 16*count)
+            })
+        }
+    }
+
+    var funcHide = function(item){
+        show = false;
+        $("nav.site-nav").find("a.pure-button.sub-button").each(function(count, item) {
+            setTimeout(function() { $(item).addClass("nav-hidden") }, 16*count)
         })
-    }).mouseleave(function(item){
-        $(item.target).find("a.pure-button.sub-button").css({'opacity': 0, 'left': '-50px'})
+    };
+
+    var timeout; //use this to track the timeouts
+    $("nav.site-nav")
+    .mouseover(function (item){
+        clearTimeout(timeout)
+        funcShow(item)
+    }).mouseout(function(item) {
+        clearTimeout(timeout)
+        timeout = setTimeout(function() { funcHide(item) }, 4000)
     })
 
+    $("nav.site-nav .home-button").click(function(item){
+        funcShow(item)
+        return false;
+    })
 })
